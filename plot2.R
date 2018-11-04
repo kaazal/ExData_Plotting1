30c93eafb5f00f6bf2d1701d370086261df2cfb6
kaazal <43524019+kaazal@users.noreply.github.com>
2018-11-03 23:17:04 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,5 +3,5 @@ names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","V
 spower <- subset(power,power$Date=="1/2/2007" | power$Date=="2/2/2007")
 spowerm <- mutate(spower,GlobalPower= as.numeric(as.character(spower$Global_active_power)), DateTime = as.POSIXct(paste(spower$Date, spower$Time, sep=" "), format = "%d/%m/%Y %H:%M:%S", tz = ""))
 plot(spowerm$DateTime, spowerm$GlobalPower, xlab="",ylab="Global Active Power(kilowatts)", type="l")
-
-
+png("plot2.png",width=480, height=480)
+dev.off()
